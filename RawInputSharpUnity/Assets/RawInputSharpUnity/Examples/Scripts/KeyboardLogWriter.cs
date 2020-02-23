3ac894265be6647d23f404f69a588c38dfb37a69
malaybaku <njslyr.trpg+harriet@gmail.com>
2020-02-23 14:19:53 +0900

    import project

---
+++ RawInputSharpUnity/Assets/RawInputSharpUnity/Examples/Scripts/KeyboardLogWriter.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using Linearstar.Windows.RawInput;
+
+namespace RawInputSharpUnity.Example
+{
+    [RequireComponent(typeof(KeyboardRawInput))]
+    public class KeyboardLogWriter : MonoBehaviour
+    {
+        [SerializeField] private LogToUi logToUi = null;
+
+        private void Start()
+        {
+            GetComponent<KeyboardRawInput>().ReceiveRawInputKeyboardData += OnReceiveKeyboardData;
+        }
+
+        private void OnReceiveKeyboardData(RawInputKeyboardData data)
+        {
+            logToUi.AddLog(
+                $"Keyboard: {data.Device.ProductName}, keyCode={data.Keyboard.VirutalKey}, flag={data.Keyboard.Flags}"
+            );
+        }
+    }
+}
---
+++ RawInputSharpUnity/Assets/RawInputSharpUnity/Scripts/MouseRawInput.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using UnityEngine;
+using Linearstar.Windows.RawInput;
+
+namespace RawInputSharpUnity
+{
+    /// <summary>
+    /// Read all mouse input event from RawInput.
+    /// </summary>
+    public class MouseRawInput : MonoBehaviour
+    {
+        [Tooltip("Set true if you want to read input during the application is in background")]
+        [SerializeField] private bool readWhenBackground = true;
+        
+        /// <summary>
+        /// Fire when receive raw input keyboard data.
+        /// </summary>
+        public event Action<RawInputMouseData> ReceiveRawInputMouseData;
+
+        private void OnEnable()
+        {
+            RawInputWindowProcedure.Instance.ReceiveRawInput += OnReceiveRawInput;
+#if UNITY_EDITOR
+            Debug.Log("RawInputDevice.RegisterDevice was skipped in editor, readWhenBackground = " + readWhenBackground);
+#else
+            RawInputDevice.RegisterDevice(
+                HidUsageAndPage.Mouse, 
+                readWhenBackground 
+                    ? RawInputDeviceFlags.InputSink | RawInputDeviceFlags.NoLegacy 
+                    : RawInputDeviceFlags.NoLegacy,
+                WinApiUtils.GetUnityWindowHandle()
+            );            
+#endif
+        }
+
+        private void OnDisable()
+        {
+            RawInputWindowProcedure.Instance.ReceiveRawInput -= OnReceiveRawInput;
+#if UNITY_EDITOR
+            Debug.Log("RawInputDevice.UnregisterDevice was skipped in editor");
+#else
+            RawInputDevice.UnregisterDevice(HidUsageAndPage.Mouse);
+#endif
+        }
+
+        private void OnReceiveRawInput(RawInputEventData data)
+        {
+            if (RawInputData.FromHandle(data.LParam) is RawInputMouseData mouseData)
+            {
+                ReceiveRawInputMouseData?.Invoke(mouseData);
+            }
+        }
+    }
+}
